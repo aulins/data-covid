9b1ae7d7e6f67945f7f35a0aa9feffb715b68063
Aulia Intan Shafira <aulins12.ais@gmail.com>
2025-07-17 12:03:31 +0700

    menambahkan data dummy

---
+++ insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO kasus_covid (wilayah, tanggal, kasus_baru, sembuh, meninggal)
+VALUES 
+('DKI Jakarta', '2021-01-01', 200, 150, 10),
+('Jawa Barat', '2021-01-01', 180, 140, 5),
+('DKI Jakarta', '2021-01-02', 220, 160, 12);
