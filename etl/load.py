a3b501eff1371b404c78cd9cbd0f18899e370961
Aulia Intan Shafira <aulins12.ais@gmail.com>
2025-07-17 22:59:17 +0700

    menambahkan requirements dan mengedit .env

--- etl/load.py
+++ etl/load.py
@@ -1,8 +1,25 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import os
 
-engine = create_engine("postgresql://postgres:bismillah@localhost:5432/covid_data")
+# Load variabel dari .env
+load_dotenv()
 
+# Ambil koneksi database dari environment
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Cek jika DATABASE_URL tidak ditemukan
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL tidak ditemukan di file .env")
+
+# Buat koneksi engine PostgreSQL
+engine = create_engine(DATABASE_URL)
+
+# Baca CSV
 df = pd.read_csv('data/cleaned/covid_cleaned.csv')
+
+# Simpan ke PostgreSQL
 df.to_sql("covid_stats", con=engine, if_exists='replace', index=False)
+
+print("✅ Data berhasil dimuat ke tabel 'covid_stats'")
